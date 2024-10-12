2955f01aacd23d1e5dfa12246f92e289e3accee8
Gomers16 <ggyt12830@gmail.com>
2024-10-12 04:02:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// Importamos el módulo express
 const express = require('express');
+
+// Creamos una aplicación express
 const app = express();
+
+// Definimos el puerto en el que escuchará la aplicación
 const port = process.env.PORT || 3000;
 
+// Definimos una ruta para manejar las solicitudes a la raíz "/"
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo desde Vercel con Express!');
+  // Enviamos una respuesta al cliente
+  res.send('¡Hola, mundo desde Vercel!');
 });
 
+// Hacemos que la aplicación escuche en el puerto definido
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  // Imprimimos un mensaje en la consola para confirmar que el servidor está funcionando
+  console.log(`Servidor corriendo en http://localhost:${port}/`);
 });
